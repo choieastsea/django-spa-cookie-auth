6376a67d820d913dbcf653811aeb7cf6ca10db13
choieastsea <choieastsea@gmail.com>
2023-01-11 15:05:19 +0900

    feat: OnetoOne Custom User Model

--- django_react_cross_origin/backend/djangocookieauth/settings.py
+++ django_react_cross_origin/backend/djangocookieauth/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
+    'my_user',
 ]
 
 MIDDLEWARE = [
